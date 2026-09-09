f5f02d9e2cc3fa7346b904c87417c528c9599156
Deepraj <95718611+dprj03@users.noreply.github.com>
2026-09-09 16:06:21 +0530

    feat: hisab privacy terms home ppf salary sip tax guides/[slug]

---
+++ src/app/hisab/privacy/page.tsx
@@ -0,0 +1,5 @@
+import { SITES } from "@/lib/sites";
+import { PrivacyPage } from "@/components/LegalPages";
+export default function Page() {
+  return <PrivacyPage site={SITES.hisab} />;
+}
---
+++ src/app/hisab/terms/page.tsx
@@ -0,0 +1,5 @@
+import { SITES } from "@/lib/sites";
+import { TermsPage } from "@/components/LegalPages";
+export default function Page() {
+  return <TermsPage site={SITES.hisab} />;
+}
